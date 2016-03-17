d88468227e21f5cb638043c8f6ae03c737aca6e2
Roman Kolesnev <rvkolesnev@gmail.com>
2016-03-17 20:22:08 +0300

    Add entrypoint

--- config/application.rb
+++ config/application.rb
@@ -31,5 +31,12 @@ module EvergridRails
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    # Disable unnecessary generators
+    config.generators do |g|
+      g.helper false
+      g.stylesheets false
+      g.javascripts false
+    end
   end
 end
